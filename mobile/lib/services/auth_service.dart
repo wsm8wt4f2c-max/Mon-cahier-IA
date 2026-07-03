eab2d012fecb34e19b402618c63f607c914a3a54
wsm8wt4f2c-max <wsm8wt4f2c@privaterelay.appleid.com>
2026-07-04 00:46:40 +0100

    Create auth_service.dart

---
+++ mobile/lib/services/auth_service.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> register(String email, String password) async {
+    final result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  Future<User?> login(String email, String password) async {
+    final result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+
+  Stream<User?> get user => _auth.authStateChanges();
+}
