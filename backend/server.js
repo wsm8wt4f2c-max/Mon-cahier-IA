43159c5d1df348e040617ae34fe83c1e0e31d227
wsm8wt4f2c-max <wsm8wt4f2c@privaterelay.appleid.com>
2026-07-04 02:21:00 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Backend running on port " + PORT);
 });
+const paymentRoutes = require("./src/routes/payment.routes");
+
+app.use("/api/payment", paymentRoutes);
