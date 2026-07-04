828805c68f0903cf9b5e172af3ab66d2b49168be
wsm8wt4f2c-max <wsm8wt4f2c@privaterelay.appleid.com>
2026-07-04 01:55:33 +0100

    Update home_screen.dart

--- mobile/lib/screens/screens/home_screen.dart
+++ mobile/lib/screens/screens/home_screen.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'ai_screen.dart';
 import 'camera_screen.dart';
+import 'premium_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final auth = AuthService();
+    final AuthService auth = AuthService();
 
     return Scaffold(
       appBar: AppBar(
@@ -61,12 +62,30 @@ class HomeScreen extends StatelessWidget {
               width: double.infinity,
               child: ElevatedButton.icon(
                 icon: const Icon(Icons.camera_alt),
-                label: const Text("Caméra Canevas (OCR)"),
+                label: const Text("Caméra Canevas"),
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (_) => const CameraScreen(),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                icon: const Icon(Icons.star),
+                label: const Text("Premium"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const PremiumScreen(),
                     ),
                   );
                 },
